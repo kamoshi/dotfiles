1d9519f14673699e5e5a3b6f13e31a1e053aade4
Maciej Jur <maciej@kamoshi.org>
2023-07-10 22:33:59 +0200

    Add completion

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,15 +1,19 @@
 return function() 
   local configs = require("lspconfig")
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
   local on_attach = function(client, buf_number)
     local buf_opts = { noremap = true, silent = true, buffer = buf_number }
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, buf_opts)
   end
 
   configs.tsserver.setup({
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities,
   })
   configs.astro.setup({
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities,
   })
 end
 
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,8 +1,10 @@
 return {
+  -- Telescope
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     config = require "configs/treesitter",
@@ -11,14 +13,48 @@ return {
       vim.treesitter.language.register("markdown", "mdx")
     end
   },
+  -- LSP Configs
   {
     "neovim/nvim-lspconfig",
     config = require "configs/lspconfig",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp"
+    },
   },
+  -- Snippet engine
+  {
+    "L3MON4D3/LuaSnip",
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+  },
+  -- Completion
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "hrsh7th/cmp-buffer",
+  },
+  {
+    "hrsh7th/cmp-path",
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = require "configs/cmp",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+  },
+  -- Rust tools
   {
     "simrat39/rust-tools.nvim",
     config = require "configs/rust-tools",
   },
+  -- Haskell tools
   {
     "mrcjkb/haskell-tools.nvim",
     dependencies = {
@@ -27,15 +63,18 @@ return {
     },
     branch = "1.x.x",
   },
+  -- Discord presence
   {
     "andweeb/presence.nvim",
     config = require "configs/presence",
   },
+  -- Status line
   {
     "nvim-lualine/lualine.nvim",
     config = require "configs/lualine",
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
+  -- Editor theme
   {
     "rebelot/kanagawa.nvim",
     init = function()
